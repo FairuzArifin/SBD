d522397882b669cb1f32399976b7b9313b728763
FairuzArifin <89765817+FairuzArifin@users.noreply.github.com>
2022-05-25 20:50:34 +0700

    Upload Punya Rahman - Bagian 2
    Upload Punya Rahman - Bagian 2

--- includes/function.php
+++ includes/function.php
@@ -1,16 +1,49 @@
 <?php
 include 'connect.php';
 
-function data_bid($bid){
-    $query = "SELECT * FROM nft WHERE nft_id = $bid";
+//function show
+function show($query) {
 
-    $h = mysqli_query($connect, $query);
-        $nft = array();
+    global $connect;
 
-    // ... tiap baris dari hasil query dikumpulkan ke $nft
-        while ($row = mysqli_fetch_assoc($h)) {
-        $nft[] = $row;
+    $box = [];
+
+    $result = mysqli_query($connect, $query);
+
+    while($row = mysqli_fetch_assoc($result)) {
+
+        $box[] = $row;
+
     }
-    // ... lanjut di tampilan
-    return $nft;
+
+    return $box;
+}
+
+//Register
+function regist($data) {
+
+    global $connect;
+
+    $name = strtolower(stripslashes($data['name']));
+    $username = stripslashes($data['username']);
+    $email = stripslashes($data['email']);
+    //$dob = $data['dob'];
+    //$age = $data['age'];
+    $pass = mysqli_real_escape_string($connect, $data["password"]);
+    $pass2 = mysqli_real_escape_string($connect, $data["password_confirmation"]);
+    //$coutry = stripslashes($data['country']);
+    //$address = stripslashes($data['address']);
+
+    //passwordconfirmation
+    if ($pass !== $pass2) {
+        return false;
+    }
+
+    //Paswword Hasing
+    $pass = password_hash($pass, PASSWORD_DEFAULT);
+
+    //insert data ke table user
+    $daftar = mysqli_query($connect, "INSERT INTO user(username, email, password, name, about, date_of_birth, age, country, address) VALUES('$username', '$email', '$pass', '$name', '', '', 0, '', '')");
+
+    return mysqli_affected_rows($connect) ;
 }
