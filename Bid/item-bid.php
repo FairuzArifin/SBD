fee17ab5fb2052fb0dd813a6f2fecb4d68f82381
Jo766hi <jonatansilalahi09@gmail.com>
2022-05-26 23:36:09 +0700

    JO

--- Bid/item-bid.php
+++ Bid/item-bid.php
@@ -1,17 +1,28 @@
+
 <?php
-    include '../includes/connect.php';
-    $query = "SELECT * FROM nft
-                FULL OUTER JOIN table2
-                ON bid.nft_id = nft.nft_id
-                WHERE nft_id = $nftid
-                ";
+include '../includes/connect.php';
 
-    $h = mysqli_query($connect, $query);
-        $nft = array();
 
-    // ... tiap baris dari hasil query dikumpulkan ke $nft
-        while ($row = mysqli_fetch_assoc($h)) {
-        $nft[] = $row;
-    }
+$bid =$_POST['bid_amount'];
+$user_id = 2101;
+$nft_id = 3101;
+$check = "SELECT MAX(bid_ongoing) as max from bid_ongoing WHERE nft_id = $nft_id";
+$process = mysqli_query($connect, $check);
+$row = mysqli_fetch_array($process);
+$largestbid = $row['max'];
+if($bid > $largestbid ){
+
+    $sql = "INSERT INTO bid_ongoing (user_id,nft_id,bid_ongoing) VALUES ('$user_id', '$nft_id', '$bid')";          			                
+        if($connect->query($sql)===TRUE){
+                echo "<script>alert('Bidding Success');history.go(-1) </script>";
+           
+ } else {
+                echo "Error : " .$sql."<br/>".$connect->error;
+        }
+} else {
+    echo "<script>alert('Bid Amount Must be Higher');history.go(-1) </script>";
+}
+    
+$connect->close();
 
 ?>
--- includes/function.php
+++ includes/function.php
@@ -1,26 +1,69 @@
 <?php
-    include 'connect.php';
-    //function show
-    function show($sql2) {
-        global $connect;
-        $box = [];
-        $result = mysqli_query($connect, $sql2);
-        while($row = mysqli_fetch_assoc($result)) {
-            $box[] = $row;
-        }
-        return $box;
+//function show
+function show($sql2) {
+    global $connect;
+    $box = [];
+    $result = mysqli_query($connect, $sql2);
+    while($row = mysqli_fetch_assoc($result)) {
+        $box[] = $row;
     }
+    return $box;
+}
 
-    function user_nft($nftid){
-        global $connect;
-        $box = array();
-        $query = "SELECT * FROM kepemilikan
-                    JOIN user 
-                    ON user.user_id = kepemilikan.user_id
-                    WHERE kepemilikan.nft_id = $nftid";
-                    $result = mysqli_query($connect, $query);
-                    while($row = mysqli_fetch_assoc($result)) {
-                        $box[] = $row;
-                    }
-                    return $box;
-                }
+function user_nft($nftid){
+    global $connect;
+    $box = array();
+    $query = "SELECT * FROM kepemilikan
+                JOIN user 
+                ON user.user_id = kepemilikan.user_id
+                WHERE kepemilikan.nft_id = $nftid";
+    $result = mysqli_query($connect, $query);
+    while($row = mysqli_fetch_assoc($result)) {
+        $box[] = $row;
+    }
+    return $box;
+}
+function show_bid($nftid){
+    global $connect;
+    $query = "SELECT bid_ongoing, name, bid_time from user 
+                INNER JOIN bid_ongoing
+                ON bid_ongoing.user_id = user.user_id
+                WHERE nft_id = 3101;";
+
+    $result = mysqli_query($connect, $query);
+    while($row = mysqli_fetch_assoc($result)) {
+        $box[] = $row;
+    }
+    return $box;
+
+
+}
+
+function save_bid(){
+    extract($_POST);
+    $data = "";
+    $chk = $this->db->query("SELECT * FROM bid_ongoing where nft_id = $nftid order by bid_amount desc limit 1 ");
+    $uid = $chk->num_rows > 0 ? $chk->fetch_array()['user_id'] : 0 ;
+    foreach($_POST as $k => $v){
+        if(!in_array($k, array('bid_id')) && !is_numeric($k)){
+            if(empty($data)){
+                $data .= " $k='$v' ";
+            }else{
+                $data .= ", $k='$v' ";
+            }
+        }
+    }
+                $data .= ", user_id='{$_SESSION['user_id']}' ";
+
+    if($uid == $_SESSION['user_id']){
+        echo "<p>User id tidak sama</p>";
+        exit;
+    }
+    if(empty($id)){
+        $save = $this->db->query("INSERT INTO bid_ongoing set ".$data);
+    }else{
+        $save = $this->db->query("UPDATE bid_ongoing set ".$data." where id=".$id);
+    }
+    if($save)
+        return 1;
+    }
