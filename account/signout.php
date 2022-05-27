ab4f8476f4c3a229882a8ca7bdcf211a795895bb
FairuzArifin <yuuarifin@gmail.com>
2022-05-27 09:04:50 +0700

    Update signout.php

--- account/signout.php
+++ account/signout.php
@@ -1,14 +1,6 @@
-<<<<<<< Updated upstream:account/signout.php
 <?php
 session_start();
 if(session_destroy()){
     header("Location: ../index.php");
 }
-=======
-<?php
-session_start();
-if(session_destroy()){
-    header("Location: signin.php");
-}
->>>>>>> Stashed changes:Source Code/account/signout.php
 ?>
