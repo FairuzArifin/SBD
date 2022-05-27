738ae54177ec7a78365e9250bcdaac3ef80d9779
erligurning03 <erligurning03@gmail.com>
2022-05-27 08:07:58 +0700

    Update signout.php
    ffsfsd

--- account/signout.php
+++ account/signout.php
@@ -1,6 +1,14 @@
+<<<<<<< Updated upstream:account/signout.php
 <?php
 session_start();
 if(session_destroy()){
     header("Location: ../index.php");
 }
+=======
+<?php
+session_start();
+if(session_destroy()){
+    header("Location: signin.php");
+}
+>>>>>>> Stashed changes:Source Code/account/signout.php
 ?>
