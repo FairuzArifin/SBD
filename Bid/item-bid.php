38e9bec13e75f31fbc2499b6912db95d3f29e851
FairuzArifin <yuuarifin@gmail.com>
2022-06-01 14:11:28 +0700

    Ubah Wallet jadi User

--- Bid/item-bid.php
+++ Bid/item-bid.php
@@ -3,16 +3,23 @@ include '../includes/connect.php';
 
 $bid =$_POST['bid_amount'];
 $user_id = 2101;
-$nft_id = 3101;
+$nft_id = 9;
+
 $check = "SELECT MAX(bid_ongoing) AS max FROM bid_ongoing WHERE nft_id = $nft_id";
-$time = "SELECT auction_end AS minn FROM bid WHERE nft_id = $nft_id"; //2
 $process = mysqli_query($connect, $check);
-$proces = mysqli_query($connect, $time); //2
 $row = mysqli_fetch_array($process);
-$row2 = mysqli_fetch_assoc($proces); //2
-$www = $row2['minn'];
 $largestbid = $row['max'];
+
+$time = "SELECT auction_end AS minn FROM bid WHERE nft_id = $nft_id";
+$proces = mysqli_query($connect, $time);
+$row2 = mysqli_fetch_array($proces);
+$www = $row2['minn'];
 $now = date("Y-m-d h:i:sa");
+
+$author = "SELECT user_id FROM kepemilikan WHERE nft_id = $nft_id";
+$proce = mysqli_query($connect, $author);
+$row3 = mysqli_fetch_array($proce);
+
 if(strtotime($www) > strtotime($now)){
     if($bid > $largestbid ){
         $sql = "INSERT INTO bid_ongoing (user_id,nft_id,bid_ongoing) VALUES ('$user_id', '$nft_id', '$bid')";          			                
@@ -22,15 +29,14 @@ if(strtotime($www) > strtotime($now)){
             echo "Error : " .$sql."<br/>".$connect->error;
         }
     } else {
-        echo "<script>alert('$now');history.go(-1) </script>";
+        echo "<script>alert('Your Bid Must be Higher');history.go(-1) </script>";
     } 
 } else {
-        $status = "SELECT nft_id FROM nft WHERE nft_id = $nft_id";
-        $process3 = mysqli_query($connect, $status);
-        $row3 = mysqli_fetch_array($process3);
-        $sql = "INSERT INTO nft_id (nft_id) VALUES ('1')";          			                
+        $sql = "UPDATE `nft` SET `status_nft` = '1' WHERE `nft`.`nft_id` = $nft_id";          			                
             if($connect->query($sql)===TRUE){
                 echo "<script>alert('NFT Already Sold');history.go(-1) </script>";
+            } else {
+                echo "Gagal";
             }
 }
     
